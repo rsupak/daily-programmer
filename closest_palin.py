7a17c6fad56b3fb9928997e1f4d2f56b1acc937c
Richard Supak <richard.supak@icloud.com>
2018-08-12 22:21:28 -0500

    added description to closest_palin.py

--- closest_palin.py
+++ closest_palin.py
@@ -1,8 +1,17 @@
-def count_digits(num):
-    count = 0
-    while num > 0:
-        count += 1
-        num //= 10
-    return count
+"""
+*Closest Palindrome*
+Given an integer n, find the closest integer (not including itself), which is a palindrome.
 
+The 'closest' is defined as absolute difference minimized between two integers.
 
+```Input: "123"
+Output: "121"
+
+Input: "99800"
+Output: "99799"
+
+Input: "10"
+Output: "9"```
+The input n is a positive integer represented by string, whose length will not exceed 18.
+If there is a tie, return the smaller one as answer. (edited)
+"""
