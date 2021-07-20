a35e45a9d1269d726fba05aa7bffa90d0e09171a
Richard Supak <richard.supak@icloud.com>
2021-07-20 16:21:28 -0500

    add drag and drop demo app

---
+++ daily-html/file-upload/main.js
@@ -0,0 +1,55 @@
+'use strict';
+
+let dropArea = document.getElementById('drop-area');
+;['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+    dropArea.addEventListener(eventName, preventDefaults, false);
+})
+
+function preventDefaults (e) {
+    e.preventDefault();
+    e.stopPropagation();
+}
+
+;['dragenter', 'dragover'].forEach(eventName => {
+    dropArea.addEventListener(eventName, highlight, false);
+})
+
+;['dragleave', 'drop'].forEach(eventName => {
+    dropArea.addEventListener(eventName, unhighlight, false);
+})
+
+function highlight(e) {
+    dropArea.classList.add('highlight');
+}
+
+function unhighlight(e) {
+    dropArea.classList.remove('highlight');
+}
+
+dropArea.addEventListener('drop', handleDrop, false);
+
+function handleDrop(e) {
+    let dt = e.dataTransfer;
+    let files = dt.files;
+
+    handleFiles(files);
+}
+
+function handleFiles(files) {
+    ([...files]).forEach(uploadFile);
+    files.forEach(uploadFile);
+}
+
+function uploadFile(file) {
+    let url = 'YOUR URL HERE';
+    let formData = new FormData();
+
+    formData.append('file', file);
+
+    fetch(url, {
+        method: 'POST',
+        body: formData
+    })
+    .then(() => { /* Done. Inform the user */ })
+    .catch(() => { /* Error. Inform the user */ })
+}
